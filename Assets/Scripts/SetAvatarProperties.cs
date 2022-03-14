9da6260cfc04963893211f923a08200f505e9efa
aleksander_trepala_boombit <aleksander.trepala@boombit.com>
2022-03-14 20:33:12 +0100

    added width param updated default values

--- Assets/Scripts/SetAvatarProperties.cs
+++ Assets/Scripts/SetAvatarProperties.cs
@@ -5,6 +5,7 @@ public class SetAvatarProperties : MonoBehaviour
 {
     [SerializeField] private bool isFemale;
     [SerializeField] private string scaleFile = "zbroja_skala.txt";
+    [SerializeField] private string widthFile = "zbroja_szerokosc.txt";
     [SerializeField] private string verticalOffsetFile = "zbroja_wysokosc.txt";
     [SerializeField] private string armScaleFile = "rece_skala.txt";
     [SerializeField] private AvatarController avatarController;
@@ -12,6 +13,7 @@ public class SetAvatarProperties : MonoBehaviour
     string VerticalOffsetFilePath => Path.Combine(Application.dataPath, verticalOffsetFile);
     string ArmScaleFilePath => Path.Combine(Application.dataPath, armScaleFile);
     string ScaleFilePath => Path.Combine(Application.dataPath, scaleFile);
+    string WidthFilePath => Path.Combine(Application.dataPath, widthFile);
 
     void Start()
     {
@@ -51,6 +53,26 @@ public class SetAvatarProperties : MonoBehaviour
                 else
                 {
                     sizeController.maleDefaultScale = scaleFactor;
+                }
+            }
+        }
+        
+        if (!string.IsNullOrEmpty(widthFile) && File.Exists(WidthFilePath))
+        {
+            var content = File.ReadAllText(WidthFilePath);
+            bool result = float.TryParse(content, out var widthFactor);
+            if (result && avatarController != null)
+            {
+                avatarScaler.bodyWidthFactor = widthFactor;
+                avatarScaler.ScaleAvatar(0,false);
+                var sizeController = FindObjectOfType<SizeController>();
+                if (isFemale)
+                {
+                    sizeController.femaleDefaultWidth = widthFactor;
+                }
+                else
+                {
+                    sizeController.maleDefaultWidth = widthFactor;
                 }
             }
         }
